6e24dd1a038fc8fa3fbc75eac93ba2694971e3aa
Séverin Lemaignan <severin.lemaignan@epfl.ch>
2013-10-17 15:59:08 +0200

    Support for 'about' in the RDFlib backend + work on NS

--- src/minimalkb/backends/rdflib_backend.py
+++ src/minimalkb/backends/rdflib_backend.py
@@ -1,15 +1,20 @@
 import logging; logger = logging.getLogger("minimalKB."+__name__);
 DEBUG_LEVEL=logging.DEBUG
 
+
 import shlex
 from rdflib import Graph, URIRef
 from rdflib.namespace import Namespace, NamespaceManager, RDF, RDFS, OWL, XSD
 
+from minimalkb.exceptions import KbServerError
+
+DEFAULT_NAMESPACE = ('chili', 'http://chili-research.epfl.ch#')
 
 class RDFlibStore:
 
     def __init__(self):
 
+        self.default_ns = Namespace(DEFAULT_NAMESPACE[1])
         self.models = {}
 
     def clear(self):
@@ -22,7 +27,7 @@ class RDFlibStore:
         """
 
         # creates a model if necessary
-        graph = self.models.setdefault(model, Graph())
+        graph = self._create_or_get_graph(model)
 
         for stmt in stmts:
             graph.add(self._parse_stmt(stmt))
@@ -41,12 +46,29 @@ class RDFlibStore:
         """ Add the given statements to the given model, updating the statements
         with a functional predicate.
         """
-        raise NotImplementedError()
+
+        logger.warn("Currently with RDFlib backend, update is strictly equivalent to " + \
+                    "add (ie, no functional property check")
+
+        self.add(stmts, model)
 
     def about(self, resource, models):
         """ Returns all statements involving the resource.
         """
-        raise NotImplementedError()
+        res = []
+        resource = self._parse_resource(resource)
+
+        for model in models:
+            if not model in self.models:
+                logger.warn("Trying to list statments from an unexisting model!")
+                return []
+
+            graph = self.models[model]
+            res += [self._format_stmt(graph, s, p, o) for s, p, o in graph.triples((resource, None, None))]
+            res += [self._format_stmt(graph, s, p, o) for s, p, o in graph.triples((None, resource, None))]
+            res += [self._format_stmt(graph, s, p, o) for s, p, o in graph.triples((None, None, resource))]
+
+        return res
 
     def has(self, stmts, models):
         """ Returns true if the statements or partial statements
@@ -61,9 +83,20 @@ class RDFlibStore:
     def classesof(self, concept, direct, models):
         """ Returns the RDF classes of the concept.
         """
+
         raise NotImplementedError()
 
     #############################################################################
+
+    def _create_or_get_graph(self,name):
+        if name not in self.models:
+            graph = Graph()
+            namespace_manager = NamespaceManager(Graph())
+            namespace_manager.bind(DEFAULT_NAMESPACE[0], self.default_ns)
+            graph.ns_manager = namespace_manager
+            self.models[name] = graph
+
+        return self.models[name]
 
     def _parse_stmt(self, stmt):
         s,p,o = shlex.split(stmt)
@@ -74,7 +107,7 @@ class RDFlibStore:
     def _parse_resource(self, resource):
         tokens = resource.split(':')
         if len(tokens) == 1:
-            return URIRef(resource)
+            return self.default_ns[resource]
         
         ns, resource = tokens
 
@@ -85,15 +118,20 @@ class RDFlibStore:
         elif ns == "owl":
             return OWL[resource]
 
-        return URIRef(resource)
-        #elif ns == "xsd":
-        #    return XSD[resource]
+        raise KbServerError("Unknown namespace <%s> for resource %s" % (ns, resource))
     
+    def _format_stmt(self, graph, s, p, o):
+
+        nsm = graph.ns_manager
+
+        stmt =  [nsm.qname(s), nsm.qname(p), nsm.qname(o)]
+        #remove ns prefix for default namespace
+        default_prefix = DEFAULT_NAMESPACE[0] + ":"
+        return [s[len(default_prefix):] if s.startswith(default_prefix) else s for s in stmt]
+
     def __str__(self):
         res = ""
         for name, graph in self.models.items():
-            res += name + "\n"
-            for s,p,o in graph:
-                res += "[" + ", ".join([s,p,o]) + "]\n"
+            res += name + "\n" + graph.serialize(format='n3') + "\n"
         return res
         
