chili-epfl/minimalkb

commit 3310b19fb5438b799a61213c10ef44a3dca4c2c5

Accept NEW_CLASS_INSTANCE event, but does not handle it yet
While here, add registerEvent compat method

src/minimalkb/kb.py
@@ -43,7 +43,9 @@ def parse_stmt(stmt):
 class Event:
 
     NEW_INSTANCE = "NEW_INSTANCE"
+    NEW_CLASS_INSTANCE = "NEW_CLASS_INSTANCE"
     NEW_INSTANCE_ONE_SHOT = "NEW_INSTANCE_ONE_SHOT"
+    NEW_CLASS_INSTANCE_ONE_SHOT = "NEW_CLASS_INSTANCE_ONE_SHOT"
 
     def __init__(self, kb, type, trigger, var, patterns, models):
         self.kb = kb
@@ -55,7 +57,7 @@ class Event:
 
         self.id =  "evt_" + str(hash(self.type + \
                     self.trigger + \
-                    self.var + \
+                    self.var if self.var else "" + \
                     str(sorted(self.patterns)) + \
                     str(sorted(self.models))))
 
@@ -375,6 +377,12 @@ class MinimalKB:
 
     @compat
     @api
+    def registerEvent(self, type, trigger, patterns):
+        return self.subscribe(type, trigger, None, patterns)
+
+
+    @compat
+    @api
     def discriminateForAgent(self, *args):
         raise NotImplementedError('discriminateForAgent not implemented in MinimalKB')
 
@@ -383,7 +391,6 @@ class MinimalKB:
     def getLabel(self, concept):
         return self.store.label(concept)
 
-    
     @compat
     @api
     def getDirectClassesOf(self, concept):

commit eb0a11971efbc4b55f86529805de94f2e49b4d27

Fixed bug in event ID generation introduced in 3310b19fb5438

src/minimalkb/kb.py
@@ -57,7 +57,7 @@ class Event:
 
         self.id =  "evt_" + str(hash(self.type + \
                     self.trigger + \
-                    self.var if self.var else "" + \
+                    (self.var if self.var else "") + \
                     str(sorted(self.patterns)) + \
                     str(sorted(self.models))))
 

testing/test.py
@@ -97,6 +97,9 @@ class TestSequenceFunctions(unittest.TestCase):
         self.assertItemsEqual(self.kb.lookup('alfred'), [['alfred', 'instance']])
         self.assertItemsEqual(self.kb.lookup('Robot'), [['Robot', 'class']])
 
+        self.kb += ["alfred likes icecream"]
+        self.assertItemsEqual(self.kb.lookup('likes'), [['likes', 'object_property']])
+
         self.kb += ["nono rdfs:label \"alfred\""]
         self.assertItemsEqual(self.kb.lookup('alfred'), [['alfred', 'instance'], ['nono', 'undecided']])
 
@@ -122,7 +125,7 @@ class TestSequenceFunctions(unittest.TestCase):
                               ['johnny'])
 
 
-    def _test_complex_queries(self):
+    def test_complex_queries(self):
         self.assertItemsEqual(self.kb["?agent rdf:type Robot", "?agent desires ?obj"], [])
 
         self.kb += ["nono rdf:type Human", "alfred rdf:type Robot"]
@@ -140,7 +143,7 @@ class TestSequenceFunctions(unittest.TestCase):
         self.assertItemsEqual(self.kb["?agent desires ?act", "?act rdf:type Action"], [{"agent":"nono", "act":"jump"}])
         self.assertItemsEqual(self.kb["?agent desires ?obj"], [{"agent":"alfred", "obj":"oil"}, {"agent":"nono", "obj":"jump"}])
 
-    def _test_update(self):
+    def test_update(self):
         self.kb += ["nono isNice true", "isNice rdf:type owl:FunctionalProperty"]
         self.assertItemsEqual(self.kb["* isNice true"], ['nono'])
 
@@ -159,7 +162,7 @@ class TestSequenceFunctions(unittest.TestCase):
             print("In callback. Got evt %s" % evt)
             eventtriggered[0] = True
 
-        self.kb.subscribe(["?o isIn room"], onevent)
+        evtid = self.kb.subscribe(["?o isIn room"], onevent)
 
         # should not trigger an event
         self.kb += ["alfred isIn garage"]
@@ -180,7 +183,8 @@ class TestSequenceFunctions(unittest.TestCase):
         self.assertFalse(eventtriggered[0])
 
         # alfred is already in garage, should not fire an event
-        self.kb.subscribe(["?o isIn garage"], onevent)
+        evtid2 = self.kb.subscribe(["?o isIn garage"], onevent)
+        self.assertFalse(evtid == evtid2)
         time.sleep(0.1)
         self.assertFalse(eventtriggered[0])
 