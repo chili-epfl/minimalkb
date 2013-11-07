chili-epfl/minimalkb

commit 15248e38df5917f81b6f2b5e1e6168b5bf0c605a

Improving 'end of life'
In particular, properly kill the subprocesses (classification and co)

bin/minimalkb
@@ -48,10 +48,10 @@ class MinimalKBChannel(asynchat.async_chat):
         logger.debug("Got request:" + self.data )
         request, args = self.parse_request(self.data)
         self.data = ""
+        self.kb.submitrequest(self, request, *args)
+
         if request == "close":
             self.close_when_done()
-        else:
-            self.kb.submitrequest(self, request, *args)
 
     def sendmsg(self, msg):
         status, res = msg

src/minimalkb/kb.py
@@ -501,6 +501,13 @@ class MinimalKB:
         self._lifespan_manager = Process(target = lifespan.start_service, args = ('kb.db',))
         self._lifespan_manager.start()
 
+    def stop_services(self):
+        self._reasoner.terminate()
+        self._lifespan_manager.terminate()
+
+        self._reasoner.join()
+        self._lifespan_manager.join()
+
     def normalize_models(self, models):
         """ If 'models' is None, [] or contains 'all', then
         returns the set of all models known to the KB.
@@ -520,6 +527,12 @@ class MinimalKB:
             return self.models
 
     def execute(self, client, name, *args):
+
+        if name == "close":
+            logger.info("Closing the knowledge base.")
+            self.stop_services()
+            return
+
         f = getattr(self, name)
         if hasattr(f, "_compat"):
                 logger.warn("Using non-standard method %s. This may be " % f.__name__ + \

commit fda3ac5678d3354a9afe685d37861bf82fccf166

Do not kill background services when a client leaves
This fixes a bug introduced in 15248e38df5 and brings back
the unit-tests to a normal state (ie, only 2 break, as expected)

bin/minimalkb
@@ -136,4 +136,5 @@ if __name__ == '__main__':
             asyncore.loop(count = 1)
             kb.process()
     except KeyboardInterrupt:
+        kb.stop_services()
         logger.info("Bye bye")

src/minimalkb/kb.py
@@ -529,8 +529,7 @@ class MinimalKB:
     def execute(self, client, name, *args, **kwargs):
 
         if name == "close":
-            logger.info("Closing the knowledge base.")
-            self.stop_services()
+            logger.info("Closing connection to client.")
             return
 
         f = getattr(self, name)

src/minimalkb/services/lifespan.py
@@ -18,7 +18,6 @@ class SQLiteLifespanManager:
     ####################################################################
     ####################################################################
     def clean(self):
-
         starttime = time.time()
 
         timestamp = datetime.datetime.now().isoformat()
@@ -34,7 +33,7 @@ class SQLiteLifespanManager:
                                     stmts_to_remove)
 
 
-            logger.debug("Cleaning %s stmts (took %fsec)." % (len(stmts_to_remove), time.time() - starttime))
+            logger.info("Cleaning %s stmts (took %fsec)." % (len(stmts_to_remove), time.time() - starttime))
 
     def __call__(self, *args):
 